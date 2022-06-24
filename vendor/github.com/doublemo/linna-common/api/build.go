b56d19a5c81b04b0010f8a9269bf045eaaa69a71
randyma <435420057@qq.com>
2022-06-24 10:47:42 +0800

    remove

--- vendor/github.com/doublemo/linna-common/api/build.go
+++
@@ -1,3 +0,0 @@
-package api
-
-//go:generate protoc -I. --go_out=. --go_opt=paths=source_relative api.proto
--- vendor/github.com/doublemo/linna-common/rtapi/build.go
+++
@@ -1,3 +0,0 @@
-package rtapi
-
-//go:generate protoc -I.. -I. --go_out=. --go_opt=paths=source_relative realtime.proto
